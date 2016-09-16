d6834d4df2e5c61f97e8937bc0d6310db6ed937c
NickEales <neales@microsoft.com>
2016-09-16 13:25:22 +1000

    Updated Update-Baselines.ps1 in Azure Automation.

--- Update-Baselines.ps1
+++ Update-Baselines.ps1
@@ -1,72 +1,97 @@
+#Azure Automation Powershell Runbook to update SCVMM Baselines with current applicable updates from WSUS.
+#written by Nick Eales.
+#Last Update 16 Sept 2016
 Param(
-	[String]$VMMServerName = "VMM",
+	[Parameter(Mandatory=$true)][String]$VMMServerName,
 	[String]$AdminAccount="AutomationUser"
 )
 
-
-
-Function Update-BaseLineUpdates{ 
+Function Update-SingleScvmmBaseline{ 
     Param ( 
-	$Wsus,
-    [Parameter(Mandatory=$false, 
-    ValueFromPipeline=$true, 
-    ValueFromPipelineByPropertyName=$true, 
-    ValueFromRemainingArguments=$false, 
-    Position=0)] 
-    [String] 
-    $BaseLineName
+        [Parameter(Mandatory=$true,Position=0)] 
+        [String] $BaseLineName,
+        [Parameter(Mandatory=$true,Position=1)]
+        [Microsoft.UpdateServices.Internal.BaseApi.Update[]]$AllUpdates
     ) 
   
-    write-output "Starting Baseline '$BaselineName'"
+    write-output "Starting Baseline '$BaselineName'" -foregroundcolor Green
 
     $baseline = Get-SCBaseline -Name $BaseLineName 
-    write-output "$($baseline.UpdateCount) : Current updates in Baseline $BaseLineName"
-
-    write-verbose "Get list of updates from WSUS for this classification" 
-    $AllUpdates=$Wsus.GetUpdates() | where UpdateClassificationTitle -eq $baselineName | where {$_.IsSuperseded -eq $False -and $_.IsApproved -eq $true -and $_.isBeta -eq $false -and $_.isdeclined -eq $false -and $_.islatestrevision -eq $true -and $_.publicationstate -ne "Expired" -and $_.title -notmatch "itanium"}
-
-    write-verbose "$($AllUpdates.count) Updates before filtering"
-    $OperatingSystems = get-scvmhost | Where {$_.operatingSystem -match "(\S+\sServer\s\d+\s\S\d)|(\S+\sServer\s\d+)|(\S+\sServer)"} | %{$matches[0]}  | select -unique
-    write-verbose "Filtering to operating systems: $($OperatingSystems -join ',')"
-
-    $OSUpdates = @()
-    foreach($os in $OperatingSystems){
-        $OSUpdates += $allupdates | where {$_.ProductTitles -contains $OS -or $_.ProductTitles -notmatch "^Windows" -or $_.ProductTitles -match "$OS$"}
+    if($Baseline -eq $NULL){
+        Write-output "Baseline '$BaselineName' not found - adding to VMM with all managed computers in scope"
+        Add-Baseline -BaseLineName $BaseLineName
     }
-    $AllUpdates = $OSUpdates
-    write-verbose "$($allupdates.count) Updates found for included operating systems."
+    $BaselineUpdates = $AllUpdates | where UpdateClassificationTitle -eq $baselineName
 
-    write-verbose "Filter list of updates to those not currently in baseline"
-    $AddedUpdates = $allupdates | where {$baseline.Updates.updateid -notcontains $_.id.updateid.guid}
+    write-output "$($baseline.UpdateCount) : Current updates in Baseline '$BaseLineName'"
+
+    write-verbose "$($BaselineUpdates.count) Updates found of classification '$baselineName' that are approved in WSUS and not superseded or expired"
+
+    #Filter list of updates to those not currently in baseline
+    $AddedUpdates = $BaselineUpdates | where {$baseline.Updates.updateid -notcontains $_.id.updateid.guid}
+    #Convert list to type that we can use for Set-SCBaseline
     $addedUpdateList = get-scupdate | where {$AddedUpdates.id.updateid.guid -contains $_.updateid.GUID}
 
-    write-output "$($addedUpdateList.Count) : New updates to add in $BaselineName"
+    write-output "$($addedUpdateList.Count) : New updates to add in to baseline '$BaseLineName'"
     if(($addedUpdateList| measure).count -gt 0){
         Set-SCBaseline -Baseline $baseline -Name $BaseLineName -Description $BaseLineName -AddUpdates $addedUpdateList  | ft ObjectType,name,updatecount -autosize
     }
      
     write-verbose "Scan WSUS for Updates that should not be Checked anymore"  
-    $removeUpdateList = $baseline.Updates | Where-Object -Property UpdateClassification -EQ -Value $BaseLineName | where {$AllUpdates.id.updateid.guid -notcontains $_.updateid}
+    $removeUpdateList = $baseline.Updates | Where {$BaselineUpdates.id.updateid.guid -notcontains $_.updateid}
 
-    write-output "$($removeUpdateList.count) : Updates to remove from $baselineName"
+    write-output "$($removeUpdateList.count) : Updates to remove from baseline '$BaseLineName'"
     if(($removeUpdateList | measure).count -gt 0){
         Set-SCBaseline -Baseline $baseline -Name $BaseLineName -Description $BaseLineName -RemoveUpdates $RemoveupdateList  | ft ObjectType,name,updatecount -autosize
     }
 } 
 
-	#Check for required features and modules
-	if(-not (get-WindowsFeature -name UpdateServices-API).installed){
-		write-output "Please run 'install-windowsfeature -name UpdateServices-API' on ''$($ENV:Computername)''"
-		throw "WSUS Powershell module not installed"
-	}else{write-verbose "WSUS PowerShell Module found"}
-	if((get-module -list | where name -match virtualmachinemanager).count -eq 0){
-		Throw "Virtual Machine Manager powershell module not installed on $($ENV:Computername)"
-	}else{write-verbose "Virtual Machine Manager powershell module found"}
+Function Get-FilteredListOfUpdates{
+    param([Microsoft.UpdateServices.Internal.BaseApi.UpdateServer]$wsus)
+    #directly connecting to WSUS for list of update - This gives far more reliable filtering than using SCVMM 
+
+    $AllUpdates=$Wsus.GetUpdates() | 
+        where {
+            $_.IsSuperseded -eq $False -and 
+            $_.IsApproved -eq $true -and 
+            $_.isBeta -eq $false -and 
+            $_.isdeclined -eq $false -and 
+            $_.islatestrevision -eq $true -and 
+            $_.publicationstate -ne "Expired" -and 
+            $_.title -notmatch "itanium" 
+        }
+
+    # Get list of operating systems in use on the computers managed by SCVMM
+    # - will use WMI to query some managed computers for OS version. 
+    $VMHostOSs=get-scvmhost | Where {$_.operatingSystem -match "(\S+\sServer\s\d+\s\S\d)|(\S+\sServer\s\d+)|(\S+\sServer)"} | %{$matches[0]}
+    $VMMManagedComputerOSs = get-scvmmmanagedcomputer | where role -ne Host | select -unique FQDN | where {(Get-WmiObject -class Win32_OperatingSystem -ComputerName $_.FQDN -ErrorAction SilentlyContinue -WarningAction SilentlyContinue).caption -match "(\S+\sServer\s\d+\s\S\d)|(\S+\sServer\s\d+)|(\S+\sServer)"} | %{$matches[0]}
+    $OperatingSystems = @($VMHostOSs) + @($VMMManagedComputerOSs) | select -Unique
+
+    write-verbose "Filtering to operating systems: $($OperatingSystems -join ',')"
+    $UpdatesToUse = @()
+    foreach($os in $OperatingSystems){
+        $UpdatesToUse += $allupdates | where {$_.ProductTitles -contains $OS -or $_.ProductTitles -notmatch "^Windows" -or $_.ProductTitles -match "$OS$"}
+    }
+    write-verbose "$($UpdatesToUse.count) Updates found for included operating systems."
+
+    return($UpdatesToUse)
+}
 
 write-output "Running on $($ENV:Computername)"
+
+#Check for required features and modules
+if(-not (get-WindowsFeature -name UpdateServices-API).installed){
+	write-output "Please run 'install-windowsfeature -name UpdateServices-API' on ''$($ENV:Computername)''"
+	throw "WSUS Powershell module not installed"
+}else{write-verbose "WSUS PowerShell Module found"}
+if((get-module -list | where name -match virtualmachinemanager).count -eq 0){
+	Throw "Virtual Machine Manager powershell module not installed on $($ENV:Computername)"
+}else{write-verbose "Virtual Machine Manager powershell module found"}
+
 write-output "Getting credential to use"
 $AdminCred = Get-AutomationPSCredential -Name $AdminAccount
 if($AdminCred -eq $NULL){throw "Unable to get Automation Credential '$AdminAccount'"}	
+
 write-output "Connecting to VMM Server '$VMMServerName' as '$($AdminCred.Username)' (From $adminAccount Credential Asset)"
 $VMMServer = Get-SCVMMServer -Computername $VMMServerName -Credential $AdminCred
 if($VMMServer -eq $null){Throw "Unable to connect to VMMServer $VMMServerName"}
@@ -80,11 +105,10 @@ $SCUpdateServer | Start-SCUpdateServerSynchronization | FL ServerType,UpstreamSe
 $wsus = Get-WSUSServer -name $SCUpdateServer.Name -portnumber $SCUpdateServer.Port
 if($wsus -eq $NULL){Throw "unable to connect to WSUS server $($SCUpdateServer.Name):$($SCUpdateServer.Port)"}
 
-Update-BaseLineUpdates -wsus $Wsus -baselinename "Security Updates"  
-Update-BaseLineUpdates -wsus $Wsus -baselinename "Critical Updates"
-Update-BaseLineUpdates -wsus $Wsus -baselinename "Updates"
-Update-BaseLineUpdates -wsus $Wsus -baselinename "Update Rollups"
-Update-BaseLineUpdates -wsus $Wsus -baselinename "Hotfix"
+Write-output "Getting list of current applicable updates for the operating systems managed by VMM"
+$FilteredUpdates = Get-FilteredListOfUpdates -wsus $wsus
+
+@("Security Updates","Critical Updates","Updates","Update Rollups", "Hotfix") | %{Update-SingleScvmmBaseline -BaseLineName $_ -allupdates $FilteredUpdates}
  
 write-output "Start Compliance Scan for all Servers"  
 Get-SCVMMManagedComputer | Start-SCComplianceScan -RunAsynchronously | select name,overallcompliancestate,statusstring
